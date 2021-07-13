d8da1a5bd39e6d6932ece0624cd8d9045f29c394
henrique-laet <henriquebaslaet@hotmail.com>
2021-07-13 19:56:49 -0300

    Navegacao do Site

--- _scripts/script.js
+++ _scripts/script.js
@@ -162,3 +162,42 @@ $('footer').waypoint(function(direcao) {
 },{
     offset: '470px;'
 })
+
+$('header h1').waypoint(function(direcao) {
+    if ( direcao == 'down') {
+        $('nav').addClass('navEstatico');
+        $('nav div').addClass('logoEstatico');
+        $('nav ul').addClass('menuEstatico');
+        $('nav ul li a').addClass('itemEstatico');
+        $('nav ion-icon').addClass('iconeEstatico');
+    } else {
+        $('nav').removeClass('navEstatico');
+        $('nav div').removeClass('logoEstatico');
+        $('nav ul').removeClass('menuEstatico');
+        $('nav ul li a').removeClass('itemEstatico');
+        $('nav ion-icon').removeClass('iconeEstatico');
+    }
+},{
+    offset: '200px'
+})
+
+// acoes de navegacao
+$('nav div').click(function() {
+    $('html,body').animate({scrollTop:$('header').offset().top},1000);
+})
+
+$('nav ul li:eq(0)').click(function() {
+    $('html,body').animate({scrollTop:$('section#domingo-perfeito').offset().top},1000);
+})
+
+$('nav ul li:eq(1)').click(function() {
+    $('html,body').animate({scrollTop:$('section#pizza').offset().top},1000);
+})
+
+$('nav ul li:eq(2)').click(function() {
+    $('html,body').animate({scrollTop:$('section#testemunha').offset().top},1000);
+})
+
+$('nav ul li:eq(3)').click(function() {
+    $('html,body').animate({scrollTop:$('section#plano-principal').offset().top},1000);
+})
